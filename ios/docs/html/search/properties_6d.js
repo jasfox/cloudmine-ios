6c1bebda6ec7d9d6ca92af96fa40457026dc5fec
Conrad Kramer <conrad@kramerapps.com>
2012-08-20 17:14:20 -0400

    Removed docs from version control
    (They're already in version control!)

--- ios/docs/html/search/properties_6d.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['mimetype',['mimeType',['../interface_c_m_file.html#aab7d91955bfa36611e43befca9d15c01',1,'CMFile']]]
-];
--- ios/docs/html/search/properties_6e.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['networkqueue',['networkQueue',['../interface_c_m_web_service.html#a07aafe0a4b2668b2b6f75cee245d1434',1,'CMWebService']]]
-];
