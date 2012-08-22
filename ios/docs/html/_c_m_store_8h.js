4224d9859909e3580eadbccaeccd51db358711cf
Conrad Kramer <conrad@kramerapps.com>
2012-08-22 11:34:20 -0400

    Generate a DocSet upon build
    And remove HTML docs from version control

--- ios/docs/html/_c_m_store_8h.js
+++
@@ -1,4 +0,0 @@
-var _c_m_store_8h =
-[
-    [ "CMStoreObjectDeletedNotification", "_c_m_store_8h.html#ae1dc3641b79831d9516226beaedc054f", null ]
-];
--- ios/docs/html/search/properties_64.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['distance',['distance',['../interface_c_m_distance.html#aeeff1d138fa9745778aa8bab6fdac4d0',1,'CMDistance']]]
-];
